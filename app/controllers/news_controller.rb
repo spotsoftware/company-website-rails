257d58f797bc52ad774b40fe1f9278d922f80cda
Alessandro Molari <alessandro.molari@spot.it>
2014-05-16 18:46:21 +0200

    News Title

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,6 +3,7 @@ class NewsController < ApplicationController
   AVAILABLE_NEWS = ['google-glass']
 
   def show
+    @title = t('news.googleglass.title')
     @parts = OpenStruct.new(has_navbar?: true, has_footer?: false, has_social?: false, has_analytics?: true)
     unless params[:id] && AVAILABLE_NEWS.include?(params[:id])
       raise ActionController::RoutingError.new('news unavailable')
